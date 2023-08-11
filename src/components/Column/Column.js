e6343dbe425832924332118f97ca25475fb8e8ca
Harsh Rathore <harshrathore7514@gmail.com>
2023-08-11 23:45:38 +0530

    some changes

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -83,15 +83,17 @@ function Column({colId, index }) {
             <>
                 {
                     currColTasks.map((task, index) => {
-                        // Replace this with the TaskCard component later
-                        return (
-                            <TaskCard
-                                key={task.id}
-                                currTaskColId={colId}
-                                task={task}
-                                index={index}
-                            />
-                        );
+                        if (task) { // Add this conditional check
+                            return (
+                                <TaskCard
+                                    key={task.id}
+                                    currTaskColId={colId}
+                                    task={task}
+                                    index={index}
+                                />
+                            );
+                        }
+                        return null;
                     })
                 }
             </>
